0361dffe70dbbdbcb8440c2be88a02ec882ab70f
Navin Karkera <navin@opencraft.com>
2024-07-30 20:26:25 +0530

    fix: remove support for 3.8

--- setup.py
+++ setup.py
@@ -117,7 +117,6 @@ setup(
     long_description_content_type='text/markdown',
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Framework :: Django',
@@ -130,5 +129,5 @@ setup(
     },
     packages=['multi_problem_xblock'],
     package_data=package_data("multi_problem_xblock", ["static", "templates", "public", "translations"]),
-    python_requires=">=3.8",
+    python_requires=">=3.11",
 )
